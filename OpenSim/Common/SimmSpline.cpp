23d6f79ece1a1a034d66e5a0e26e73cbb3d7d415
Antoine Falisse <antoinefalisse@gmail.com>
2021-07-12 11:07:29 -0700

    revert change simmspline

--- OpenSim/Common/SimmSpline.cpp
+++ OpenSim/Common/SimmSpline.cpp
@@ -568,7 +568,6 @@ osim_double_adouble SimmSpline::calcValue(const Vector& x) const
         /* Do a binary search to find which two points the abscissa is between. */
         i = 0;
         j = n;
-
         while (1)
         {
             k = (i+j)/2;
@@ -659,7 +658,6 @@ osim_double_adouble SimmSpline::calcDerivative(const std::vector<int>& derivComp
         /* Do a binary search to find which two points the abscissa is between. */
         i = 0;
         j = n;
-
         while (1)
         {
             k = (i+j)/2;
